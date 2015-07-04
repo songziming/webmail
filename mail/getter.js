2011f5b9216fdc92dd8f59483f73169d5d178788
songziming <s.ziming@hotmail.com>
2015-07-05 03:32:46 +0800

    added attachment support

--- mail/getter.js
+++ mail/getter.js
@@ -1,9 +1,21 @@
 var Listener = require('mail-listener2');
 var dbhelper = require('./dbhelper');
+var crypto = require('crypto');
+var path = require('path');
+var cheerio = require('cheerio');
 
 var listener;
+var attachment_dir = '/tmp';
+
+function md5sum(str) {
+    var sum = crypto.createHash('md5');
+    sum.update(str);
+    return sum.digest('hex');
+}
 
 exports.initConfig = function(mail) {
+    attachment_dir = mail.attachmentsDir;
+
     listener = new Listener({
         username: mail.auth.mailaddr,
         password: mail.auth.password,
@@ -15,7 +27,7 @@ exports.initConfig = function(mail) {
         searchFilter: ['UNSEEN'],
         markSeen: true,
         fetchUnreadOnStart: true,
-        mailParserOptions: {streamAttachments: true},
+        mailParserOptions: {streamAttachments: false},
         attachments: true,
         attachmentOptions: {directory: 'attachments/'}
     });
@@ -31,20 +43,44 @@ exports.initConfig = function(mail) {
     });
 
     listener.on("mail", function(mail, seqno, attributes) {
-        // console.log(mail);
+        // get DOM of html content        
+        var $ = cheerio.load(mail.html);
+
+        // process attachments
+        for (var i = 0; i < mail.attachments.length; ++i) {
+            var att = mail.attachments[i];
+            if (att.fileName == 'ATT00001') {
+                // if this attachment is created by Exchange, just ignore it.
+                continue;
+            }
+            var name = md5sum(att.content.toString());
+
+            // when saving file on server, we don't care about file extensions
+            var of = fs.createWriteStream(path.join('/tmp', name));
+            of.write(att.content);
+            of.end(function() { console.log('save attachment done.'); });
+
+            // change the references if needed
+            if (!!att.contentId) {
+                var targets = $('[src="cid:'+att.contentId+'"]');
+                targets.attr('src', '/attachments/'+name);
+            }
+        }
+
         // write the mail received into database
+        // TODO: add support for attachments
         console.log('adding new mail to database');
         dbhelper(
             mail.subject || '',
             mail.from[0].name + '<' + mail.from[0].address + '>',
             mail.text || '',
-            mail.html || ''
+            $.html()
         );
     });
 
-    listener.on("attachment", function(attachment) {
-        console.log(attachment);
-    });
+    // listener.on("attachment", function(attachment) {
+    //     console.log(attachment);
+    // });
 };
 
 exports.startMailGetter = function() {
