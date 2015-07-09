1e1d8eef4ceb25fbbd311988bb2afeadb9f3afb1
wungcq <wangchunqibuaa@163.com>
2015-07-09 12:34:27 +0800

    add message function, add scroll view

--- static/js/script/yourMessage.js
+++ static/js/script/yourMessage.js
@@ -2,7 +2,7 @@ define(function (require, exports, module) {
 	var juicer = require("juicer");
 	var tmp = require("tmpManager");
 	var yourMessage = function () {
-		this.listPath = '';
+		this.listPath = '/message/receive';
 		this.loadPath = '';
 		this.init();
 		return this;
@@ -14,33 +14,90 @@ define(function (require, exports, module) {
 			me.wrapperTemplate = tmp('msg-wrapper');
 			me.itemTemplate = tmp('msg-list-item');
 			me.render();
-//			me.autoFresh();
+			me.bind();
+			me.autoFresh();
+
 		},
 		render: function () {
 			var me = this;
 			$("body").append(me.wrapperTemplate);
-
 		},
-		autoFresh : function() {
+		bind: function () {
 			var me = this;
-			var str = "    Highcharts出色的图表性能得到了越来越多的使用者的认可，然而在中文环境中，Highcharts相关资源并不多，目前还没有比较完善的的系列教程。鉴于此，Highchars中文网管理团队决定编写一套详细的、完善的入门教程，帮助大家学习和使用Highcharts。";
-
-			var s = setInterval(function(){
-				var rand = parseInt(Math.random()*100);
-				var _str = str.slice(0,rand);
-				var data = {
-					msg: {
-						"time":"系统信息:2015-7-9",
-						"content": _str
+			$("#your-messages").on("click", function (e) {
+				if (e.target.classList.contains("msg")) {
+					var tar = $(e.target);
+					if(tar.hasClass("load-more")){
+						return;
 					}
-				};
-				var html = juicer(me.itemTemplate, data);
+					var title = tar.children(".time").html();
+					var content = tar.children(".content").html();
+					ALERT(title, content, function () {
+						me.removeMsg(tar);
+					});
+				}else if(e.target.parentNode.classList.contains("msg")){
+					$(e.target.parentNode).trigger("click");
+				}
+			});
+			$(".header .user-block .user-menu").click(function(){
+				if($("#your-messages").hasClass("show")){
+					me.hide();
+				}else {
+					me.show();
+				}
+			});
+		},
+		autoFresh: function () {
+			var me = this;
+			me.receive(null, me.latest || null, function (res) {
+				var html = juicer(me.itemTemplate, res);
 				$(html).insertBefore("#your-messages .list-wrapper .msg:first-child");
-			},1000);
+			});
 		},
 		list: function () {
 			var me = this;
+		},
+		show: function () {
+			var me = this;
+			$("#your-messages").addClass("show");
+		},
+		hide: function () {
+			var me = this;
+			$("#your-messages").removeClass("show");
+		},
+		receive: function (oldMessage, lastMessage, callback) {
+			var me = this;
+			var data = {
+				limit: 20
+			};
+
+			if (oldMessage != null || oldMessage != undefined) {
+				data.oldMessage = oldMessage;
+			}
+			if (lastMessage != null || lastMessage != undefined) {
+				data.lastMessage = lastMessage;
+			}
+
+			$.post(me.listPath, data, 'json').done(function (res) {
+
+				if (res.status == 1) {
+					res.messages = res.messages.reverse();
+					me.old = res.old = res.messages[ 0 ] && res.messages[ 0 ].id;
+					me.latest = res.latest = res.messages[ 0 ] && res.messages[ res.messages.length - 1 ].id;
+					callback && callback(res);
+				} else {
+					ALERT('提示', res.msg);
+				}
+			});
+		},
+		removeMsg: function (tar) {
+			tar && tar.addClass("remove");
+			var s = setTimeout(function () {
+				tar.remove();
+				clearTimeout(s);
+			}, 400);
 		}
+
 	};
 	module.exports = yourMessage;
 });
