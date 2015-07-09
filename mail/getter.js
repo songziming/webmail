3b0ffb3b457a61bf94fa4a417307bc59f8ff512b
songziming <s.ziming@hotmail.com>
2015-07-10 00:50:23 +0800

    added support for attachments download.

--- mail/getter.js
+++ mail/getter.js
@@ -2,7 +2,7 @@ var Listener = require('mail-listener2');
 var dbhelper = require('./dbhelper');
 var crypto = require('crypto');
 var path = require('path');
-var cheerio = require('cheerio');
+//var cheerio = require('cheerio');
 var fs = require('fs');
 
 var listener;
@@ -50,13 +50,14 @@ exports.initConfig = function (mail) {
 
         // process attachments
         if (!!mail.attachments) {
+            var appended = '';
             for (var i = 0; i < mail.attachments.length; ++i) {
                 var att = mail.attachments[i];
-                if (att.fileName == 'ATT00001') {
-                    // if this attachment is created by Exchange, just ignore it.
-                    continue;
-                }
-                var name = md5sum(att.content.toString());
+                // if (att.fileName == 'ATT00001') {
+                //     // if this attachment is created by Exchange, just ignore it.
+                //     continue;
+                // }
+                var name = md5sum(att.content.toString())+'_'+att.generatedFileName;
 
                 // when saving file on server, we don't care about file extensions
                 var of = fs.createWriteStream(path.join(attachment_dir, name));
@@ -64,11 +65,16 @@ exports.initConfig = function (mail) {
                 of.end(function() { console.log('save attachment done.'); });
 
                 // change the references if needed
-                // if (!!att.contentId) {
-                //     var targets = $('[src="cid:'+att.contentId+'"]');
-                //     targets.attr('src', '/attachments/'+name);
-                // }
+                if (!!att.contentId && !!mail.html) {
+                    var p = new RegExp('cid:'+att.contentId, 'g');
+                    mail.html = mail.html.replace(p, '/attachments/'+name);
+                    // var targets = $('[src="cid:'+att.contentId+'"]');
+                    // targets.attr('src', '/attachments/'+name);
+                }
+                // appended += '<p>Attachment '+att.generatedFileName+': <form method="get" action="/attachments/'+name+'"><button type="submit">Download!</button></form>';
+                appended += '<p>Attachment '+att.generatedFileName+': <a href="/attachments/'+name+'" download>download</a></p>';
             }
+            mail.html += appended;
         }
         
         // write the mail received into database
