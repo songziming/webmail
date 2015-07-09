7c2aa3534ce6a4fb9e0f526ec2b60e64a6648c9c
songziming <s.ziming@hotmail.com>
2015-07-09 14:08:57 +0800

    added support for sending mail with picture inline.

--- mail/sender.js
+++ mail/sender.js
@@ -1,5 +1,7 @@
 var mailer = require('nodemailer');
 var dbhelper = require('./dbhelper');
+var path = require('path');
+var fs = require('fs');
 
 var transporter;
 var mailOptions;
@@ -40,10 +42,34 @@ exports.initConfig = function(mail) {
 <img src="cid:lmysaaby">
 */
 
+var uefiles = require('../config').ueditor || '/uefiles';
+
 exports.sendMail = function(to, subject, html, cb) {
+    // before we actually send email, first find all occurance of local file.
+    var p = new RegExp(uefiles+'/[0-9]+\\.[A-Za-z]+', 'g');
+    var encoded = html.replace(p, function(s, i, all) {
+        var fn = path.join(__dirname, 'static', s);
+        var bf = fs.readFileSync(fn);
+        var bs = bf.toString('base64');
+        var src = 'data:image/';
+        switch (path.extname(s).toLowerCase()) {
+        case '.png':
+            src += 'png';
+            break;
+        case '.gif':
+            src += 'gif';
+            break;
+        default:
+            src += 'jpeg';
+            break;
+        }
+        src += ';base64,' + bs;
+        return src;
+    });
+
     mailOptions.to = to;
     mailOptions.subject = subject;
-    mailOptions.html = html;
+    mailOptions.html = encoded;
     console.log(mailOptions);
     transporter.sendMail(mailOptions, cb);
 };
--- ued.js
+++ ued.js
@@ -3,27 +3,29 @@ var path = require('path');
 var uefiles = require('./config').ueditor || '/uefiles';
 
 module.exports = ueditor(path.join(__dirname, 'static'), function(req, res, next) {
+    console.log(req.query.action);
+    console.log(req.ueditor);
     switch (req.query.action) {
     case 'uploadimage':
         var img = req.ueditor;
         console.log(img.filename);
         console.log(img.encoding);
         console.log(img.mimetype);
-        res.ue_up(uefiles+'/images');
+        res.ue_up(uefiles);
         break;
-    case 'uploadfile':
-        var f = req.ueditor;
-        console.log(f.filename);
-        console.log(f.encoding);
-        console.log(f.mimetype);
-        res.ue_up(uefiles+'/files');
-        break;
+    // case 'uploadfile':
+    //     var f = req.ueditor;
+    //     console.log(f.filename);
+    //     console.log(f.encoding);
+    //     console.log(f.mimetype);
+    //     res.ue_up(uefiles+'/files');
+    //     break;
     case 'listimage':
-        res.ue_list(uefiles+'/images');
+        res.ue_list(uefiles);
         break;
-    case 'listfile':
-        res.ue_list(uefiles+'/files');
-        break;
+    // case 'listfile':
+    //     res.ue_list(uefiles+'/files');
+    //     break;
     default:
         res.setHeader('Content-Type', 'application/json');
         res.redirect('/ueditor/config.json');
